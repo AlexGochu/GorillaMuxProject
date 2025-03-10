17c7172d3c26185cf1b15f244fa7a395ece6b98e
AlexGochu <gochualex@gmail.com>
2025-03-10 23:57:27 +0000

    Corrects Post response according to PR commit, where POST must return structure instead of message

--- orm.go
+++ orm.go
@@ -7,3 +7,8 @@ type Task struct {
 	Task   string `json:"task"`    // Наш сервер будет ожидать json c полем text
 	IsDone bool   `json:"is_done"` // В GO используем CamelCase, в Json - snake
 }
+type TaskResponse struct {
+	ID     uint   `json:"id"`
+	Task   string `json:"task"`
+	IsDone bool   `json:"is_done"`
+}
