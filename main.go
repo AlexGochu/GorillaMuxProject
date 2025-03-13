eec92728f69a46c6db12d128d8e2a49ff5450203
AlexGochu <gochualex@gmail.com>
2025-03-13 23:34:39 +0000

    Creates PATCH and DELETE CRUD methods

--- main.go
+++ main.go
@@ -1,70 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
-
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	var tasks []Task // Use a slice to store multiple tasks
-
-	// Query all tasks from the database
-	result := DB.Find(&tasks)
-	if result.Error != nil {
-		// Handle database error
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, `{"error": "Failed to retrieve tasks"}`)
-		return
-	}
-
-	// Set the response content type to JSON
-	w.Header().Set("Content-Type", "application/json")
-
-	// Encode tasks into JSON and write to the response
-	err := json.NewEncoder(w).Encode(tasks)
-	if err != nil {
-		// Handle JSON encoding error
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, `{"error": "Failed to encode tasks to JSON"}`)
-		return
-	}
-
-}
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-	// Decode the request body into a Task struct
-	var req Task
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		// Handle JSON decoding errors
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-
-	// Validate the request data
-	if req.Task == "" {
-		respondWithError(w, http.StatusBadRequest, "Task field is required")
-		return
-	}
-
-	// Create the task in the database
-	result := DB.Create(&req)
-
-	if result.Error != nil {
-		// Log the error for debugging
-		log.Printf("Database error: %v", result.Error)
-
-		// Handle database errors
-		respondWithError(w, http.StatusInternalServerError, "Failed to create task")
-		return
-	}
-	var taskResponse Task = req
-
-	// Respond with success
-	respondWithJSON(w, http.StatusCreated, taskResponse)
-}
 
 func main() {
 	InitDB()
@@ -74,5 +13,8 @@ func main() {
 
 	router.HandleFunc("/api", GetHandler).Methods(http.MethodGet)
 	router.HandleFunc("/api", PostHandler).Methods(http.MethodPost)
+	router.HandleFunc("/api/{id}", PatchHandler).Methods(http.MethodPatch)
+	router.HandleFunc("/api/{id}", DeleteHandler).Methods(http.MethodDelete)
+
 	http.ListenAndServe(":8080", router)
 }
