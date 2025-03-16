15abc6ed13af62170f29ae9c9f18df0ab37aaf83
AlexGochu <gochualex@gmail.com>
2025-03-16 21:10:26 +0000

    Refactors project's structure

---
+++ cmd/app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"GorillaMuxProject/internal/database"
+	"GorillaMuxProject/internal/handlers"
+	"GorillaMuxProject/internal/taskService"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	database.InitDB()
+	database.DB.AutoMigrate(&taskService.Task{})
+	repo := taskService.NewTaskRepository(database.DB)
+	service := taskService.NewService(repo)
+	handler := handlers.NewHandler(service)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/tasks", handler.GetTasksHandler).Methods(http.MethodGet)
+	router.HandleFunc("/api/tasks", handler.PostTaskHandler).Methods(http.MethodPost)
+	router.HandleFunc("/api/tasks/{id}", handler.PatchTaskHandler).Methods(http.MethodPatch)
+	router.HandleFunc("/api/tasks/{id}", handler.DeleteTaskHandler).Methods(http.MethodDelete)
+
+	http.ListenAndServe(":8080", router)
+}
